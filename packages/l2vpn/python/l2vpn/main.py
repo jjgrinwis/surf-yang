6e56fa6a5831c63773b7ae3cd6566615903fe6b0
jjgrinwis <john@grinwis.com>
2018-06-07 11:18:35 +0200

    initial commit

---
+++ packages/l2vpn/python/l2vpn/main.py
@@ -0,0 +1,96 @@
+# -*- mode: python; python-indent: 4 -*-
+import ncs
+from ncs.application import Service
+import ncs.experimental
+import id_allocator
+#import id_allocator.id_allocator as id_allocator
+
+
+# ------------------------
+# SERVICE CALLBACK EXAMPLE
+# ------------------------
+class ServiceCallbacks(Service):
+
+    # The create() callback is invoked inside NCS FASTMAP and
+    # must always exist.
+    @Service.create
+    def cb_create(self, tctx, root, service, proplist):
+        self.log.info('Service create(service=', service._path, ')')
+
+        vpn_name = service.name
+        pw_id = None
+
+        service_path = "/services/l2vpn[name='{}']".format(service.name)
+
+        # now ask for a l2vpn id from resource alloctorpool l2vpn
+        id_allocator.id_request(service, service_path, tctx.username, 'pw-id', vpn_name, False)
+
+        pw_id = id_allocator.id_read(tctx.username, root, 'pw-id', vpn_name)
+
+        if pw_id is None:
+            self.log.info('pw-id does not exit, waiting for redeploy') 
+            return
+
+        self.log.info('we received a pw-id: ', pw_id)
+
+        # we're writing our collected pw_id as operational data in our service model
+        # we're using tctx.username and system as AAA context 
+        path = "{0}/pw-id".format(service._path)
+        with ncs.maapi.single_write_trans(tctx.username, 'system', db=ncs.OPERATIONAL) as t:
+            t.set_elem(pw_id, path)
+            t.apply()
+            self.log.info('pw-id added as operational data to model')
+        
+        vars = ncs.template.Variables()
+        vars.add('DUMMY', '127.0.0.1')
+        template = ncs.template.Template(service)
+        template.apply('l2vpn-template', vars)
+
+    # The pre_modification() and post_modification() callbacks are optional,
+    # and are invoked outside FASTMAP. pre_modification() is invoked before
+    # create, update, or delete of the service, as indicated by the enum
+    # ncs_service_operation op parameter. Conversely
+    # post_modification() is invoked after create, update, or delete
+    # of the service. These functions can be useful e.g. for
+    # allocations that should be stored and existing also when the
+    # service instance is removed.
+
+    # @Service.pre_lock_create
+    # def cb_pre_lock_create(self, tctx, root, service, proplist):
+    #     self.log.info('Service plcreate(service=', service._path, ')')
+
+    # @Service.pre_modification
+    # def cb_pre_modification(self, tctx, op, kp, root, proplist):
+    #     self.log.info('Service premod(service=', kp, ')')
+
+    # @Service.post_modification
+    # def cb_post_modification(self, tctx, op, kp, root, proplist):
+    #     self.log.info('Service premod(service=', kp, ')')
+
+
+# ---------------------------------------------
+# COMPONENT THREAD THAT WILL BE STARTED BY NCS.
+# ---------------------------------------------
+class Main(ncs.application.Application):
+    def setup(self):
+        # The application class sets up logging for us. It is accessible
+        # through 'self.log' and is a ncs.log.Log instance.
+        self.log.info('Main RUNNING')
+
+        # Service callbacks require a registration for a 'service point',
+        # as specified in the corresponding data model.
+        #
+        self.register_service('l2vpn-servicepoint', ServiceCallbacks)
+
+        # If we registered any callback(s) above, the Application class
+        # took care of creating a daemon (related to the service/action point).
+
+        # When this setup method is finished, all registrations are
+        # considered done and the application is 'started'.
+
+    def teardown(self):
+        # When the application is finished (which would happen if NCS went
+        # down, packages were reloaded or some error occurred) this teardown
+        # method will be called.
+
+        self.log.info('Main FINISHED')
